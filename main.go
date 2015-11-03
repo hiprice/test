fb1ec6f203a54d4327daa849697daf86276ddf9a
jinyf <jyf0231@163.com>
2015-11-03 18:45:42 +0800

    test response

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 
 	"github.com/chanxuehong/wechat/mp"
 	"github.com/chanxuehong/wechat/mp/menu"
+	"github.com/chanxuehong/wechat/mp/message/request"
+	"github.com/chanxuehong/wechat/mp/message/response"
 )
 
 const (
@@ -20,6 +22,8 @@ const (
 )
 
 func main() {
+
+	responseText()
 	log.Println("Wechat Service: Start!")
 	http.HandleFunc("/", procRequest)
 	http.HandleFunc("/menu", createMenu)
@@ -87,4 +91,34 @@ func createMenu(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("ok")
 	w.Write([]byte("create menu:success"))
+}
+
+func ErrorHandler(w http.ResponseWriter, r *http.Request, err error) {
+	log.Println(err.Error())
+}
+
+// 文本消息的 Handler
+func TextMessageHandler(w http.ResponseWriter, r *mp.Request) {
+	// 简单起见，把用户发送过来的文本原样回复过去
+	text := request.GetText(r.MixedMsg) // 可以省略, 直接从 r.MixedMsg 取值
+	resp := response.NewText(text.FromUserName, text.ToUserName, text.CreateTime, text.Content)
+	//mp.WriteRawResponse(w, r, resp) // 明文模式
+	mp.WriteAESResponse(w, r, resp) // 安全模式
+}
+
+func responseText() {
+
+	messageServeMux := mp.NewMessageServeMux()
+	messageServeMux.MessageHandleFunc(request.MsgTypeText, TextMessageHandler) // 注册文本处理 Handler
+
+	// 下面函数的几个参数设置成你自己的参数: oriId, token, appId
+	mpServer := mp.NewDefaultServer("", "", "", nil, messageServeMux)
+
+	mpServerFrontend := mp.NewServerFrontend(mpServer, mp.ErrorHandlerFunc(ErrorHandler), nil)
+
+	// 如果你在微信后台设置的回调地址是
+	//   http://xxx.yyy.zzz/wechat
+	// 那么可以这么注册 http.Handler
+	http.Handle("/wechat", mpServerFrontend)
+	http.ListenAndServe(":80", nil)
 }
